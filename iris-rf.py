34b3000e0b660c995eec60841a6ea357a8c419a1
Mohit Kumar <mkr9395@gmail.com>
2024-10-17 00:53:17 +0530

    without adding dagshub

---
+++ iris-rf.py
@@ -0,0 +1,63 @@
+import mlflow
+import mlflow.sklearn
+from sklearn.datasets import load_iris
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, confusion_matrix
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+
+# Load the iris dataset
+iris = load_iris()
+X = iris.data
+y = iris.target
+
+# Split the dataset into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Define the parameters for the Random Forest model
+max_depth = 1
+n_estimators = 100
+
+# set up a tracking server
+
+
+# mlflow
+
+mlflow.set_experiment('iris-rf')
+
+with mlflow.start_run():
+    
+    rf = RandomForestClassifier(max_depth=max_depth, n_estimators=n_estimators)
+    
+    rf.fit(X_train, y_train)
+    
+    y_pred = rf.predict(X_test)
+    
+    accuracy = accuracy_score(y_pred=y_pred, y_test=y_test)
+    
+    mlflow.log_metric('artifact',accuracy)
+    mlflow.log_params({'max_depth':max_depth,'n_estimators':n_estimators})
+    
+    
+    cm = confusion_matrix(y_test, y_pred)
+    plt.figure(figsize=(6,6))
+    sns.heatmap(cm, annot=True, fmt='d', cmap='Blues', xticklabels=iris.target_names, yticklabels=iris.target_names)
+    plt.ylabel('Actual')
+    plt.xlabel('Predicted')
+    plt.title('Confusion Matrix')
+    
+    plt.savefig('confusion_matrix.png')
+    
+    mlflow.log_artifact('confusion_matrix.png')
+    
+    # log code
+    mlflow.log_artifact(__file__)
+    
+    # log model
+    mlflow.sklearn.log_model(rf, 'random_forest_model')
+    
+    print('accuracy',accuracy)
+    
